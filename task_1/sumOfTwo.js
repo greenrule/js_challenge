7e292d777bf7a30698e423472396cca4856454df
greenrule <sms2004@yandex.ru>
2017-11-03 12:50:49 +0500

    Update sumOfTwo.js
    add decision

--- task_1/sumOfTwo.js
+++ task_1/sumOfTwo.js
@@ -1,3 +1,7 @@
-function sumOfTwo(array1, array2, x) {
-
+const sumOfTwo = (arr1, arr2, n) => { 
+  return arr1.map(elem => {
+    return arr2.map(elem2 => elem2 + elem);
+  }).join(',')
+  	.split(',')
+  	.includes(n+'');
 }
